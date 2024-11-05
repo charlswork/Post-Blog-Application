d2ee10551bb86604625ae919666970fe57cda1ec
charles lucero <charlswork8@gmail.com>
2024-11-05 23:53:18 +0800

    pushing this web blog application 11/05/2024

---
+++ date.js
@@ -0,0 +1,11 @@
+exports.getFormattedDate = function () {
+    const now = new Date();
+    return now.toLocaleString("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+      hour: "numeric",
+      minute: "2-digit",
+    });
+  };
+  
